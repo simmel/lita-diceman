377d3b21cc69bd9bd8e6fbda34a9223477379514
Simon Lundström <simmel@soy.se>
2015-05-13 21:19:03 +0200

    Add a "real" test for !dice

--- spec/lita/handlers/diceman_spec.rb
+++ spec/lita/handlers/diceman_spec.rb
@@ -4,4 +4,11 @@ describe Lita::Handlers::Diceman, lita_handler: true do
 
     it { is_expected.to route_command("!dice answer one;answer two").to(:dice!) }
 
+    it "gives an random answer when using !dice" do
+        answers = ["answer one", "answer two", "answer three"]
+        send_message("!dice " + answers.join(";"))
+        reply = $1 if replies.last =~ /^the dice commands: (.*)/
+        expect(answers.include?(reply))
+    end
+
 end
