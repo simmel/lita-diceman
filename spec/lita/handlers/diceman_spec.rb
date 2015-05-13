0f4bdf8b20758bdf47faf9de8b241085a353ebd5
Simon Lundström <simmel@soy.se>
2015-05-13 20:30:35 +0200

    Update spec to non-deprecated

--- spec/lita/handlers/diceman_spec.rb
+++ spec/lita/handlers/diceman_spec.rb
@@ -1,5 +1,7 @@
 require "spec_helper"
 
 describe Lita::Handlers::Diceman, lita_handler: true do
-    it { routes_command("!dice answer one;answer two").to(:dice!) }
+
+    it { is_expected.to route_command("!dice answer one;answer two").to(:dice!) }
+
 end
