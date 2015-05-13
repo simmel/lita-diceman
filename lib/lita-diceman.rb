82512b7f859905278d679b87cca01165321d7d55
Simon Lundström <simmel@soy.se>
2015-05-13 21:04:10 +0200

    Respond with a random answer

--- lib/lita-diceman.rb
+++ lib/lita-diceman.rb
@@ -1,4 +1,5 @@
 require "lita"
+require 'securerandom'
 
 module Lita
   module Handlers
@@ -7,7 +8,8 @@ module Lita
       route(/^!dice\s+(.*)/, :dice!, help: { "!dice answer one;answer two[;…]" => "Gives you a random answer." })
 
       def dice!(response)
-        response.reply(response.match_data.captures.join.split(/ ?; ?/).inspect)
+        answers = response.match_data.captures.join.split(/ ?; ?/)
+        response.reply("the dice commands: " + answers[SecureRandom.random_number(answers.size)])
       end
 
     end
