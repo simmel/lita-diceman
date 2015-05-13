d73ff86e090c6fff00302e39fefdd73f35d400be
Simon Lundström <simmel@soy.se>
2015-05-13 23:10:49 +0200

    Add ?dice and it's route

--- lib/lita-diceman.rb
+++ lib/lita-diceman.rb
@@ -6,9 +6,15 @@ module Lita
     class Diceman < Handler
 
       route(/^!dice\s+(.*)/, :dice!, help: { "!dice answer one;answer two[;…]" => "Gives you a random answer." })
+      route(/^?dice\s+(.+?)\? (.*)/, :dice?, help: { "?dice Which is the correct answer? answer one;answer two[;…]" => "Gives you a random answer to your question." })
 
       def dice!(response)
         answers = response.match_data.captures.join.split(/ ?; ?/)
+        response.reply("the dice commands: " + answers[SecureRandom.random_number(answers.size)])
+      end
+
+      def dice?(response)
+        answers = response.match_data.captures[1].split(/ ?; ?/)
         response.reply("the dice commands: " + answers[SecureRandom.random_number(answers.size)])
       end
 
