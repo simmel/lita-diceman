74c641c31d99a1a05f535ccef0c2f128987e98e8
Simon Lundström <simmel@soy.se>
2015-05-13 20:55:44 +0200

    Add dice! and it's route

--- lib/lita-diceman.rb
+++ lib/lita-diceman.rb
@@ -3,6 +3,13 @@ require "lita"
 module Lita
   module Handlers
     class Diceman < Handler
+
+      route(/^!dice\s+(.*)/, :dice!, help: { "!dice answer one;answer two[;…]" => "Gives you a random answer." })
+
+      def dice!(response)
+        response.reply(response.matches.inspect)
+      end
+
     end
 
     Lita.register_handler(Diceman)
